ec9cb0e22933954136f6eaa75a13cb666ebac16c
Batmankoff <sanyayakovlev95@gmail.com>
2022-12-10 14:32:24 +0400

    added skills, about, pricing, contact, footer section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-  
+    $(".menu a").on("click", function (event) {
+    event.preventDefault();
+
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+
+    $("body,html").animate({ scrollTop: top }, 1000);
+  });
 });
